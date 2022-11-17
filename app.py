366371216e277fa3e5b7ecb57a54845625e2287d
AdityaShen <79261869+AdityaShen@users.noreply.github.com>
2022-11-17 22:52:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 import streamlit as st
 from multiapp import MultiApp
-from apps import login, data, model 
+from apps import login, data, model, analysis 
 
 app = MultiApp()
 app.add_app("Login", login.app)
 app.add_app("Data", data.app)
 app.add_app("Model", model.app)
+app.add_app("Model", analysis.app)
 app.run()
